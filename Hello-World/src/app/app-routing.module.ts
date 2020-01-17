e4cd3b25c7a855a0725c91b8b8f8be8c7b5bb652
Yuliia Kosse <juliakosee94@gmail.com>
2020-01-17 23:01:00 +0100

    Angular Routing

--- Hello-World/src/app/app-routing.module.ts
+++ Hello-World/src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AllBlogArticlesComponent } from './all-blog-articles/all-blog-articles.component';
+import { BlogArticleComponent } from './blog-article/blog-article.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'article',
+    component: BlogArticleComponent
+  }, {
+  path: '**',
+  component: AllBlogArticlesComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
